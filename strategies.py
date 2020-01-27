f68db0cebbeaa925cb36ee6f5689d992c45ef23e
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-01-27 11:52:11 +0100

    Add abstract strategy class

--- strategies.py
+++ strategies.py
@@ -1,43 +1,18 @@
 #!/usr/bin/python3
 
+from abc import ABC, abstractmethod
 from utils import floatCloseInRW
 
-class SerializedBraketsStrategy:
-    def __init__(self, _center, _step, _interval, _amount, _balancesTokenA, _balancesTokenB):
-        """
-        The strategy defines _amount trading brackets, each with price ranging from some p to p+_step.
-        The distance from one p to the next is _interval.
-        The brackets are centered around _center.
-        For every bracket (p, p+_step), the strategy works as follows:
-        if the price is lower than p, buy.
-        if the price is higher than p+_step, sell.
-        """
-        assert _amount >= 1, "Need at least one bracket"
-        assert len(_balancesTokenA) == _amount == len(_balancesTokenB), "Need to specify balance for each bracket"
-        self.center = _center
-        self.step = _step
-        self.interval = _interval
-        self.amount = _amount
-        self.startingBalancesTokenA = _balancesTokenA[::]
-        self.startingBalancesTokenB = _balancesTokenB[::]
-        self.balancesTokenA = _balancesTokenA[::]
-        self.balancesTokenB = _balancesTokenB[::]
-        self.tradeExecuted = False
-        self.getBracketValues()
+class Strategy(ABC):
+    tradeExecuted = False
+
+    def setStartingBalances(self, _startingBalancesTokenA, _startingBalancesTokenB):
+        self.startingBalancesTokenA = _startingBalancesTokenA[::]
+        self.startingBalancesTokenB = _startingBalancesTokenB[::]
 
     def reset(self):
         self.balancesTokenA = self.startingBalancesTokenA[::]
         self.balancesTokenB = self.startingBalancesTokenB[::]
-
-    def getBracketValues(self):
-        leftmostIntervalStart = self.center - (self.amount * self.interval) // 2
-        gapIntervalBracket = self.interval - self.step
-        leftmostbracketBuy = leftmostIntervalStart + gapIntervalBracket // 2
-        self.bracketBuy = [leftmostbracketBuy]
-        self.bracketSell = [leftmostbracketBuy + self.step]
-        for bracketIndex in range(0, self.amount):
-            self.bracketBuy += [self.bracketBuy[bracketIndex] + self.interval]
-            self.bracketSell += [self.bracketSell[bracketIndex] + self.interval]
 
     # price input is expressed as amounts of token A you get for one token B
     # the lower the price, the more token B is cheap
@@ -57,6 +32,10 @@ class SerializedBraketsStrategy:
                 self.balancesTokenB[bracketIndex] = 0
                 self.tradeExecuted = True
 
+    @abstractmethod
+    def getBracketValues(self):
+        pass
+
     def getTokenABalance(self):
         return sum(self.balancesTokenA)
 
@@ -65,3 +44,34 @@ class SerializedBraketsStrategy:
 
     def thereWasATrade(self):
         return self.tradeExecuted
+
+class SerializedBraketsStrategy(Strategy):
+    def __init__(self, _center, _step, _interval, _amount, _balancesTokenA, _balancesTokenB):
+        """
+        The strategy defines _amount trading brackets, each with price ranging from some p to p+_step.
+        The distance from one p to the next is _interval.
+        The brackets are centered around _center.
+        For every bracket (p, p+_step), the strategy works as follows:
+        if the price is lower than p, buy.
+        if the price is higher than p+_step, sell.
+        """
+        assert _amount >= 1, "Need at least one bracket"
+        assert len(_balancesTokenA) == _amount == len(_balancesTokenB), "Need to specify balance for each bracket"
+        self.center = _center
+        self.step = _step
+        self.interval = _interval
+        self.amount = _amount
+        self.setStartingBalances(_balancesTokenA, _balancesTokenB)
+        self.reset()
+        self.getBracketValues()
+
+    def getBracketValues(self):
+        leftmostIntervalStart = self.center - (self.amount * self.interval) // 2
+        gapIntervalBracket = self.interval - self.step
+        leftmostbracketBuy = leftmostIntervalStart + gapIntervalBracket // 2
+        self.bracketBuy = [leftmostbracketBuy]
+        self.bracketSell = [leftmostbracketBuy + self.step]
+        for bracketIndex in range(0, self.amount):
+            self.bracketBuy += [self.bracketBuy[bracketIndex] + self.interval]
+            self.bracketSell += [self.bracketSell[bracketIndex] + self.interval]
+
