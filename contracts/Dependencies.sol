99a757755cf7950e46937a3c7df00a7d7ee42f0e
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-02-10 12:41:25 +0100

    Merge contract dependencies

--- contracts/Dependencies.sol
+++ contracts/Dependencies.sol
@@ -10,5 +10,6 @@ import "@gnosis.pm/owl-token/contracts/TokenOWLProxy.sol";
 import "@gnosis.pm/owl-token/contracts/TokenOWL.sol";
 import "@gnosis.pm/solidity-data-structures/contracts/libraries/IdToAddressBiMap.sol";
 import "@gnosis.pm/solidity-data-structures/contracts/libraries/IterableAppendOnlySet.sol";
-
-contract DevDependencies {}
+import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+import "@gnosis.pm/safe-contracts/contracts/libraries/MultiSend.sol";
+import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
