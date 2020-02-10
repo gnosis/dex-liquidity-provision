e3a5712fb24b879842e2df46941a4bc8bf031f10
Ben Smith <bh2smith@gmail.com>
2020-02-10 18:10:01 +0100

    workign with batch exchange

--- contracts/Dependencies.sol
+++ contracts/Dependencies.sol
@@ -5,11 +5,14 @@ pragma solidity ^0.5.0;
 //  contracts during development.
 //
 //  For other environments, only use compiled contracts from the NPM package.
+// Token Dependencies
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "@gnosis.pm/owl-token/contracts/TokenOWLProxy.sol";
 import "@gnosis.pm/owl-token/contracts/TokenOWL.sol";
+// Batch Exchange dependencies
 import "@gnosis.pm/solidity-data-structures/contracts/libraries/IdToAddressBiMap.sol";
 import "@gnosis.pm/solidity-data-structures/contracts/libraries/IterableAppendOnlySet.sol";
+// Gnosis Safe dependencies
 import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
 import "@gnosis.pm/safe-contracts/contracts/libraries/MultiSend.sol";
 import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
