9fe551ff04ab6a917ebd539a8371953e9a73acf2
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-02-10 13:50:02 +0100

    Remove unused file

--- contracts/Imports.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity >=0.5.0 <0.6.0;
-
-import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
-import "@gnosis.pm/safe-contracts/contracts/libraries/MultiSend.sol";
-import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
