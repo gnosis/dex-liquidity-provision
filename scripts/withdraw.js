a6e0b94613654cf686fbca8245ab7e5049b5c2a2
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-02-25 13:23:04 +0100

    Add prompt

--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -1,4 +1,4 @@
-const { signAndSend } = require("./sign_and_send")
+const { signAndSend, promptUser } = require("./sign_and_send")
 const {
   getRequestWithdraw,
   getWithdraw,
@@ -87,10 +87,13 @@ module.exports = async callback => {
       throw(new Error("No operation specified"))
     }
 
-    // careful! transaction.operation and transaction.value are ignored by signAndSend.
-    // this is fine for, since we only send transactions to multisend, but we should
-    // TODO: generalize signAndSend to accept any transaction
-    await signAndSend(masterSafe, transaction, web3, argv.network)
+    const answer = await promptUser("Are you sure you want to send this transaction to the EVM? [yN] ")
+    if (answer == "y" || answer.toLowerCase() == "yes") {
+      // careful! transaction.operation and transaction.value are ignored by signAndSend.
+      // this is fine for, since we only send transactions to multisend, but we should
+      // TODO: generalize signAndSend to accept any transaction
+      await signAndSend(masterSafe, transaction, web3, argv.network)
+    }
 
     callback()
   } catch (error) {
