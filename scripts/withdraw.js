347451a3537c3d695e25e8514d925eaf943a03ce
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-02-26 17:37:54 +0100

    Add functionality to withdraw all funds

--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -1,3 +1,6 @@
+const Contract = require("@truffle/contract")
+const BatchExchange = Contract(require("@gnosis.pm/dex-contracts/build/contracts/BatchExchange"))
+
 const { signAndSend, promptUser } = require("./sign_and_send")
 const {
   getRequestWithdraw,
@@ -15,9 +18,10 @@ const argv = require("yargs")
     type: "string",
     describe: "file name (and path) to the list of withdrawals.",
   })
-  .option("withdrawalsFromDepositFile", {
-    type: "string",
-    describe: "file name (and path) to the list of deposits whose corresponding tokens will be withdrawn.",
+  .option("allTokens", {
+    type: "boolean",
+    default: false,
+    describe: "ignore amounts from withdrawalFile and try to withdraw the maximum amount available for each trader.",
   })
   .option("requestWithdraw", {
     type: "boolean",
@@ -34,7 +38,7 @@ const argv = require("yargs")
     default: false,
     describe: "transfer back funds from traders to master. Funds must be present in the trader wallets",
   })
-  .demand(["masterSafe"])
+  .demand(["masterSafe", "withdrawalFile"])
   .help(
     "Make sure that you have an RPC connection to the network in consideration. For network configurations, please see truffle-config.js"
   )
@@ -44,36 +48,60 @@ const argv = require("yargs")
     } else if (argv.requestWithdraw && (argv.transferBackToMaster || argv.withdraw)) {
       throw(new Error("Argument error: --request cannot be used with any of --withdraw, --transferBackToMaster"))
     }
-    if ((argv.withdrawalFile && !argv.withdrawalsFromDepositFile) || (!argv.withdrawalFile && argv.withdrawalsFromDepositFile)) {
-      return true
-    } else if (argv.withdrawalFile && argv.withdrawalsFromDepositFile) {
-      throw(new Error("Argument error: pass only one of withdrawalFile, withdrawalsFromDepositFile"))
-    } else {
-      throw(new Error("Argument error: either withdrawalFile or withdrawalsFromDepositFile must be specified"))
-    }
+    return true
   })
   .version(false).argv
 
-const extractWithdrawals = function(deposits) {
-  return deposits.map(deposit => ({
-    amount: deposit.amount,
-    tokenAddress: deposit.tokenAddress,
-    traderAddress: deposit.userAddress,
-  }))
+const getAmount = async function(slaveAddress, tokenAddress, exchange) {
+  let amount
+  const ERC20 = artifacts.require("ERC20Detailed")
+  const token = await ERC20.at(tokenAddress)
+  if (argv.requestWithdraw)
+    amount = (await exchange.getBalance(slaveAddress, tokenAddress)).toString()
+  else if (argv.withdraw) {    const currentBatchId = Math.floor( Date.now() / (5 * 60 * 1000) ) // definition of BatchID, it avoids making a web3 request for each withdrawal to get BatchID
+    const pendingWithdrawal = await exchange.getPendingWithdraw(slaveAddress, tokenAddress)
+    if (pendingWithdrawal[1].toNumber() == 0) {
+      console.log("Warning: no withdrawal was requested for address", slaveAddress, "and token", (await token.name()))
+      amount = "0"
+    }
+    if ((amount != "0") && (pendingWithdrawal[1].toNumber() >= currentBatchId)) {
+      console.log("Warning: amount cannot be withdrawn from the exchange right now, withdrawing zero")
+      amount = "0"
+    }
+    amount = pendingWithdrawal[0].toString()
+  } else {
+    amount = (await token.balanceOf(slaveAddress)).toString()
+  }
+  if (amount == "0")
+    console.log("Warning: address", slaveAddress, "has no balance to withdraw for token", (await token.name()))
+  return amount
 }
 
 module.exports = async callback => {
   try {
+    await BatchExchange.setProvider(web3.currentProvider)
+    await BatchExchange.setNetwork(web3.network_id)
+    const exchange = await BatchExchange.deployed()
     const GnosisSafe = artifacts.require("GnosisSafe")
     const masterSafe = await GnosisSafe.at(argv.masterSafe)
+    
+    let withdrawals = require(argv.withdrawalFile)
 
-    let withdrawals
-    if (argv.withdrawalFile) {
-      withdrawals = require(argv.withdrawalFile)
-    } else { // argv.withdrawalsFromDepositFile is defined
-      const deposits = require(argv.withdrawalsFromDepositFile)
-      withdrawals = extractWithdrawals(deposits)
+    if (argv.allTokens) {
+      console.log("Retrieving amount of tokens to withdraw.")
+      // get full amount to withdraw from the blockchain
+      withdrawals = await Promise.all(
+        withdrawals.map(
+          async withdrawal => ({
+            traderAddress: withdrawal.userAddress,
+            tokenAddress: withdrawal.tokenAddress,
+            amount: await getAmount(withdrawal.userAddress, withdrawal.tokenAddress, exchange),
+          })
+        )
+      )
     }
+    
+    console.log("Started building withdraw transaction.")
     let transaction
     if (argv.requestWithdraw)
       transaction = await getRequestWithdraw(masterSafe.address, withdrawals, web3, artifacts)
@@ -85,6 +113,18 @@ module.exports = async callback => {
       transaction = await getWithdrawAndTransferFundsToMaster(masterSafe.address, withdrawals, web3, artifacts)
     else {
       throw(new Error("No operation specified"))
+    }
+
+    for (const withdrawal of withdrawals) {
+      const ERC20 = artifacts.require("ERC20Detailed")
+      const token = await ERC20.at(withdrawal.tokenAddress)
+      const tokenDecimals = (await token.decimals.call()).toNumber()
+      const tokenSymbol = await token.symbol.call()
+      if (tokenDecimals != 18) 
+        throw(new Error("These scripts currently only support tokens with 18 decimals."))
+
+      const unitAmount = web3.utils.fromWei(withdrawal.amount.toString(), "ether")
+      console.log(`Safe ${withdrawal.traderAddress} receiving (from ${masterSafe.address.slice(0,6)}...${masterSafe.address.slice(-2)}) and depositing ${unitAmount} ${tokenSymbol} into BatchExchange`)
     }
 
     const answer = await promptUser("Are you sure you want to send this transaction to the EVM? [yN] ")
