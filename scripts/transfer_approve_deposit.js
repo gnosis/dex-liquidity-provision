3ec95a95eef46e7a402a68884bea3131483fd89e
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-03-11 11:37:49 +0100

    Fix another skipped name change

--- scripts/transfer_approve_deposit.js
+++ scripts/transfer_approve_deposit.js
@@ -24,7 +24,7 @@ module.exports = async callback => {
     const deposits = require(argv.depositFile)
     // TODO - make a simpler to construct deposit file style
     console.log("Preparing transaction data...")
-    const transaction = await buildTransferApproveDeposit(masterSafe.address, deposits, web3, artifacts, true)
+    const transaction = await buildTransferApproveDepositFromList(masterSafe.address, deposits, web3, artifacts, true)
 
     const answer = await promptUser("Are you sure you want to send this transaction to the EVM? [yN] ")
     if (answer == "y" || answer.toLowerCase() == "yes") {
