6e3fc1dc38336c97f0d48267cc7dbe2fe6b35907
josojo <josojo@hotmail.de>
2020-03-12 17:25:56 +0100

    small correction

--- test/test-utils.js
+++ test/test-utils.js
@@ -4,8 +4,8 @@ const { toErc20Units } = require("../scripts/utils/printing_tools")
 const prepareTokenRegistration = async function(account, exchange) {
   const owlToken = await TokenOWL.at(await exchange.feeToken())
   await owlToken.setMinter(account)
-  await owlToken.mintOWL(account, toErc20Units(10))
-  await owlToken.approve(exchange.address, toErc20Units(10))
+  await owlToken.mintOWL(account, toErc20Units(10, 18))
+  await owlToken.approve(exchange.address, toErc20Units(10, 18))
 }
 
 module.exports = {
