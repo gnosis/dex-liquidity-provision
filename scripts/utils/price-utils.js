73b101899c6867878bd2615d353df319f496aedb
AlexHerrmann(josojo) <josojo@hotmail.de>
2020-03-19 17:44:15 +0100

    new deposit logic + lowest and highestLimit for bracket strategy (#87)
    -introduces the highest and lower Limit for bracket strategy
    - make a sanity check for the highest and lowest bounds 
    - adapt the funding of the bracket in compelte_liquidity_provision

--- scripts/utils/price-utils.js
+++ scripts/utils/price-utils.js
@@ -1,6 +1,104 @@
 module.exports = function(web3 = web3, artifacts = artifacts) {
   const axios = require("axios")
+  const BN = require("bn.js")
+  const exchangeUtils = require("@gnosis.pm/dex-contracts")
+
   const { fetchTokenInfoFromExchange } = require("./trading_strategy_helpers")(web3, artifacts)
+
+  const checkCorrectnessOfDeposits = async (
+    currentPrice,
+    bracketAddress,
+    exchange,
+    stableToken,
+    targetToken,
+    investmentStableTokenPerBracket,
+    investmentTargetTokenPerBracket
+  ) => {
+    const bracketExchangeBalanceStableToken = (await exchange.getBalance(bracketAddress, stableToken.address)).toString()
+    const bracketExchangeBalanceTargetToken = (await exchange.getBalance(bracketAddress, targetToken.address)).toString()
+    const auctionElements = exchangeUtils.decodeOrdersBN(await exchange.getEncodedUserOrders.call(bracketAddress))
+    const bracketOrders = auctionElements.filter(order => order.user.toLowerCase() === bracketAddress.toLowerCase())
+    assert.equal(bracketOrders.length, 2)
+
+    const stableTokenId = await exchange.tokenAddressToIdMap.call(stableToken.address)
+    const sellStableTokenOrders = bracketOrders.filter(order => order.sellToken == stableTokenId)
+    assert.equal(sellStableTokenOrders.length, 1)
+    const sellStableTokenOrder = sellStableTokenOrders[0]
+
+    const targetTokenId = await exchange.tokenAddressToIdMap.call(targetToken.address)
+    const sellTargetTokenOrders = bracketOrders.filter(order => order.sellToken == targetTokenId)
+    assert.equal(sellTargetTokenOrders.length, 1)
+    const sellTargetTokenOrder = sellTargetTokenOrders[0]
+
+    // Check that tokens with a lower price than the target price are funded with stableTokens
+    if (checkThatOrderPriceIsBelowTarget(currentPrice, sellStableTokenOrder)) {
+      // checks whether price is in middle of bracket:
+      if (checkThatOrderPriceIsBelowTarget(1 / currentPrice, sellTargetTokenOrder)) {
+        assert.isTrue(
+          checkFundingInTheMiddleBracket(
+            bracketExchangeBalanceStableToken,
+            bracketExchangeBalanceTargetToken,
+            investmentStableTokenPerBracket,
+            investmentTargetTokenPerBracket
+          )
+        )
+      } else {
+        assert.equal(bracketExchangeBalanceStableToken, investmentStableTokenPerBracket.toString())
+      }
+    } else {
+      assert.equal(bracketExchangeBalanceStableToken, 0)
+    }
+
+    if (checkThatOrderPriceIsBelowTarget(1 / currentPrice, sellTargetTokenOrder)) {
+      if (checkThatOrderPriceIsBelowTarget(currentPrice, sellStableTokenOrder)) {
+        assert.isTrue(
+          checkFundingInTheMiddleBracket(
+            bracketExchangeBalanceStableToken,
+            bracketExchangeBalanceTargetToken,
+            investmentStableTokenPerBracket,
+            investmentTargetTokenPerBracket
+          )
+        )
+      } else {
+        assert.equal(bracketExchangeBalanceTargetToken, investmentTargetTokenPerBracket.toString())
+      }
+    } else {
+      assert.equal(bracketExchangeBalanceTargetToken, 0)
+    }
+  }
+
+  const checkThatOrderPriceIsBelowTarget = function(currentPrice, order) {
+    const multiplicator = 1000000000
+    return new BN(currentPrice * multiplicator).mul(order.priceNumerator) > order.priceDenominator.mul(new BN(multiplicator))
+  }
+
+  const checkFundingInTheMiddleBracket = function(
+    bracketExchangeBalanceStableToken,
+    bracketExchangeBalanceTargetToken,
+    investmentStableTokenPerBracket,
+    investmentTargetTokenPerBracket
+  ) {
+    // For the middle bracket the funding can go in either bracket
+    // it depends on closer distance from the currentPrice to the limit prices fo the bracket-traders
+    return (
+      (bracketExchangeBalanceStableToken === "0" &&
+        bracketExchangeBalanceTargetToken === investmentTargetTokenPerBracket.toString()) ||
+      (bracketExchangeBalanceTargetToken === "0" &&
+        bracketExchangeBalanceStableToken === investmentStableTokenPerBracket.toString())
+    )
+  }
+
+  const areBoundsReasonable = function(currentPrice, lowestLimit, highestLimit) {
+    const boundsCloseTocurrentPrice = currentPrice / 1.5 < lowestLimit && highestLimit < currentPrice * 1.5
+    if (!boundsCloseTocurrentPrice) {
+      console.log("Please double check your bounds. They seem to be unreasonable")
+    }
+    const currentPriceWithinBounds = currentPrice > lowestLimit && highestLimit < currentPrice
+    if (!currentPriceWithinBounds) {
+      console.log("Please double check your bounds. Current price is not within the bounds")
+    }
+    return currentPriceWithinBounds && boundsCloseTocurrentPrice
+  }
 
   // returns undefined if the price was not available
   const getDexagPrice = async function(tokenBought, tokenSold) {
@@ -43,5 +141,7 @@ module.exports = function(web3 = web3, artifacts = artifacts) {
 
   return {
     isPriceReasonable,
+    areBoundsReasonable,
+    checkCorrectnessOfDeposits,
   }
 }
