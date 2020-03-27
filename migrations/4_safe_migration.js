21c48043f6084d2843a17e33ed9a63f7159a57b1
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-03-27 16:34:41 +0000

    Move development migrations on the same file (#112)

--- migrations/4_safe_migration.js
+++
@@ -1,9 +0,0 @@
-const GnosisSafe = artifacts.require("./GnosisSafe.sol")
-const ProxyFactory = artifacts.require("./GnosisSafeProxyFactory.sol")
-const MultiSend = artifacts.require("./MultiSend.sol")
-
-module.exports = async function(deployer) {
-  await deployer.deploy(GnosisSafe)
-  await deployer.deploy(ProxyFactory)
-  await deployer.deploy(MultiSend)
-}
