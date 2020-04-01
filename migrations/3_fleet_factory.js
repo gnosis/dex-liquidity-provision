da9f546d817dfe17cfe76dea1ff3a9fcbc9cb9a3
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-04-01 17:19:01 +0000

    FleetFactory migration (#121)
    * Add truffle-plugin-verify
    
    * Add scripts to package
    
    * Add flattener
    
    * Rewrite networks.js with right formatting
    
    * There were no contracts deployed before, remove addresses of Migrations.sol
    
    * Add Rinkeby migration addresses
    
    * Hardcode gas limit in migration scripts
    
    * Prettify
    
    * Add mainnet migration addresses

--- migrations/3_fleet_factory.js
+++ migrations/3_fleet_factory.js
@@ -2,5 +2,5 @@ const GnosisSafeProxyFactory = artifacts.require("./GnosisSafeProxyFactory.sol")
 const FleetFactory = artifacts.require("./FleetFactory.sol")
 
 module.exports = async function(deployer) {
-  await deployer.deploy(FleetFactory, GnosisSafeProxyFactory.address)
+  await deployer.deploy(FleetFactory, GnosisSafeProxyFactory.address, { gas: 500000 })
 }
