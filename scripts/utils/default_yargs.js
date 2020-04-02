988c2ee8885b88be30ab430653f97e393faa5b79
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-04-02 11:07:34 +0200

    Add network

--- scripts/utils/default_yargs.js
+++ scripts/utils/default_yargs.js
@@ -3,3 +3,8 @@ module.exports = require("yargs")
   .epilog(
     "Make sure that you have an RPC connection to the network in consideration. For network configurations, please see truffle-config.js"
   )
+  .option("network", {
+    type: "string",
+    describe: "where the scripts are executed (supported networks: rinkeby, mainnet)",
+    choices: ["rinkeby", "mainnet"],
+  })
