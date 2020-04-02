3a4fe9dd3ec27813011d3e0d74251c1ef350bc77
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-04-02 11:08:12 +0200

    Add explicit help

--- scripts/utils/default_yargs.js
+++ scripts/utils/default_yargs.js
@@ -1,6 +1,7 @@
 module.exports = require("yargs")
   .version(false)
   .strict()
+  .help("help")
   .epilog(
     "Make sure that you have an RPC connection to the network in consideration. For network configurations, please see truffle-config.js"
   )
