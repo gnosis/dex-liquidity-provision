99d45e918071b83c5c3e8d0e5c0c097d6ae69252
Ben Smith <bh2smith@gmail.com>
2020-04-07 13:31:45 +1200

    Several great suggestions.

--- scripts/synthetix/facilicate_trade.js
+++ scripts/synthetix/facilicate_trade.js
@@ -1,95 +1,86 @@
 const { getExchange } = require("../utils/trading_strategy_helpers")(web3, artifacts)
 const { SynthetixJs } = require("synthetix-js")
-const { calculateBuyAndSellAmountsFromPrice, fetchTokenInfoFromExchange } = require("../utils/trading_strategy_helpers")(web3, artifacts)
+const { calculateBuyAndSellAmountsFromPrice, fetchTokenInfoFromExchange } = require("../utils/trading_strategy_helpers")(
+  web3,
+  artifacts
+)
 
-// TODO - put this somewhere more generic. Copied from dex-contracts
-const sendTxAndGetReturnValue = async function (method, ...args) {
-  const result = await method.call(...args)
-  await method(...args)
-  return result
+const sUSDAddress = {
+  mainnet: "0xAe38b81459d74A8C16eAa968c792207603D84480",
+  rinkeby: "0x1b642a124CDFa1E5835276A6ddAA6CFC4B35d52c",
 }
 
-// truffle uses network by name and SynthetixJS uses network by ID
-const networkMap = {
-  "mainnet": 1,
-  "rinkeby": 4,
+const sETHAddress = {
+  mainnet: "0xD0DC005d31C2979CC0d38718e23c82D1A50004C0",
+  rinkeby: "0x0647b2C7a2a818276154b0fC79557F512B165bc1",
 }
-
-const argv = require("yargs")
-  .option("spread", {
-    type: "float",
-    describe: "Percentage increase required for trade (fees not accounted)",
-    default: 0.2,
-  })
-  .version(false).argv
 
 module.exports = async callback => {
   try {
-    const snxjs = new SynthetixJs({ networkId: networkMap[argv.network] })
-
+    const snxjs = new SynthetixJs({ networkId: await web3.eth.net.getId() })
     const exchange = await getExchange(web3)
-    const account = (await web3.eth.getAccounts())[0]
+    const defaultAccount = (await web3.eth.getAccounts())[0]
 
-    // Both of these hardcoded tokens have 18 decimal places.
-    // We "trust" that this will always be the case
-    // Although it seems that synthetix has the authority to upgade their token
-    // which could mean deploying a new one with a different number of decimal places.
+    // Both of these hardcoded tokens are assumed to have 18 decimal places.
+    // We "trust" that this will always be the case although it seems
+    // that synthetix reserves the authority to upgrade their token
+    // This could mean issuing a new one with a different number of decimals.
     const sETHKey = await snxjs.sETH.currencyKey()
     const sUSDKey = await snxjs.sUSD.currencyKey()
 
-    const sETHAddress = await snxjs.Synthetix.synths(sETHKey)
-    const sUSDAddress = await snxjs.Synthetix.synths(sUSDKey)
-    console.log("sETH Address", sETHAddress)
-    console.log("sUSD Address", sUSDAddress)
-    const referenceTokenId = (await exchange.tokenAddressToIdMap.call(sUSDAddress)).toNumber()
-    const etherTokenId = (await exchange.tokenAddressToIdMap.call(sETHAddress)).toNumber()
+    // Fetch token IDs and other relevant token information for order placement.
+    const referenceTokenId = (await exchange.tokenAddressToIdMap.call(sUSDAddress[process.env.NETWORK_NAME])).toNumber()
+    const etherTokenId = (await exchange.tokenAddressToIdMap.call(sETHAddress[process.env.NETWORK_NAME])).toNumber()
 
     const tokenInfoPromises = fetchTokenInfoFromExchange(exchange, [referenceTokenId, etherTokenId])
-
     const stableToken = await tokenInfoPromises[referenceTokenId]
     const targetToken = await tokenInfoPromises[etherTokenId]
-
-    // Note that sUSD is always 1 with synthetix
-    const exchangeRate = await snxjs.ExchangeRates.rateForCurrency(sETHKey)
-    const formatedRate = snxjs.utils.formatEther(exchangeRate)
-    console.log("sETH Price", snxjs.utils.formatEther(exchangeRate))
-
-    const batch_index = (await exchange.getCurrentBatchId.call()).toNumber()
 
     const buyTokens = [etherTokenId, referenceTokenId]
     const sellTokens = [referenceTokenId, etherTokenId]
 
-    const [upperSellAmount, upperBuyAmount] = calculateBuyAndSellAmountsFromPrice(
-      formatedRate * (1 + argv.spread / 100),
-      stableToken,
-      targetToken
+    // Compute Rates and Fees based on price of sETH.
+    // Note that sUSD always has a price of 1 within synthetix protocol.
+    const exchangeRate = await snxjs.ExchangeRates.rateForCurrency(sETHKey)
+    const formatedRate = snxjs.utils.formatEther(exchangeRate)
+    console.log("sETH Price", snxjs.utils.formatEther(exchangeRate))
+    
+    // Using synthetix's fees, and formatting their return values with their tools, plus parseFloat.
+    const sETHTosUSDFee = parseFloat(
+      snxjs.utils.formatEther(
+        await snxjs.Exchanger.feeRateForExchange(sETHKey, sUSDKey)
+      )
     )
+    const sUSDTosETHFee = parseFloat(
+      snxjs.utils.formatEther(
+        await snxjs.Exchanger.feeRateForExchange(sUSDKey, sETHKey)
+      )
+    )
+
+    // Compute buy-sell amounts based on unlimited orders with rates from above.
     const [lowerBuyAmount, lowerSellAmount] = calculateBuyAndSellAmountsFromPrice(
-      formatedRate * (1 - argv.spread / 100),
+      formatedRate * (1 - sUSDTosETHFee),
       stableToken,
       targetToken
     )
 
+    const [upperSellAmount, upperBuyAmount] = calculateBuyAndSellAmountsFromPrice(
+      formatedRate * (1 + sETHTosUSDFee),
+      stableToken,
+      targetToken
+    )
     const buyAmounts = [lowerBuyAmount, upperBuyAmount]
     const sellAmounts = [lowerSellAmount, upperSellAmount]
 
+    // Fetch auction index and declare validity interval for orders.
+    const batch_index = (await exchange.getCurrentBatchId.call()).toNumber()
     const validFroms = Array(2).fill(batch_index)
     const validTos = Array(2).fill(batch_index + 1)
-    
-    // TODO - use replaceOrder if possible.
-    await sendTxAndGetReturnValue(
-      exchange.placeValidFromOrders,
-      buyTokens,
-      sellTokens,
-      validFroms,
-      validTos,
-      buyAmounts,
-      sellAmounts,
-      {
-        from: account,
-      }
-    )
 
+    // TODO - use replaceOrder if possible.
+    await exchange.placeValidFromOrders(buyTokens, sellTokens, validFroms, validTos, buyAmounts, sellAmounts, {
+      from: defaultAccount,
+    })
     callback()
   } catch (error) {
     callback(error)
