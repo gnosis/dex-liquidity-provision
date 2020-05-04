ba1e345505ba8de84f6a1c114bf83085926cf496
josojo <josojo@hotmail.de>
2020-05-04 16:20:04 +0200

    changing the comments

--- scripts/utils/sign_and_send.js
+++ scripts/utils/sign_and_send.js
@@ -28,7 +28,7 @@ module.exports = function (web3 = web3, artifacts = artifacts) {
     const nonce = await masterSafe.nonce()
     const safeTxGas = 4000000 // Since the gas can not yet be estimated reliably, we are hard coding it.
     // 4000000 is a magic value: This value ensures that the user gets a gas limit proposal in Metask
-    // of roughly 6m for deployment with 20 bracket
+    // of roughly 6m - MetaMask adds a buffer of roughly 50% here.
     const baseGas = 0
     console.log("Aquiring Transaction Hash")
     const transactionHash = await masterSafe.getTransactionHash(
