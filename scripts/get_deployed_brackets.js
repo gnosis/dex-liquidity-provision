0aad19590e0a32a6b5df9d0813361a9c50ca6b46
josojo <josojo@hotmail.de>
2020-05-07 08:23:10 +0200

    more sophisticated writing into the csv with more information and addressing of the comments in the PR.

--- scripts/get_deployed_brackets.js
+++ scripts/get_deployed_brackets.js
@@ -1,30 +1,61 @@
-const FleetFactory = artifacts.require("FleetFactory")
+const createCsvWriter = require("csv-writer").createObjectCsvWriter
+const exchangeUtils = require("@gnosis.pm/dex-contracts")
+const { fetchTokenInfoFromExchange, getExchange, getDeployedBrackets } = require("./utils/trading_strategy_helpers")(
+  web3,
+  artifacts
+)
 
-const argv = require("./utils/default_yargs").option("masterSafe", {
-  type: "string",
-  describe: "Address of Gnosis Safe owning every bracket",
-  demandOption: true,
-}).argv
+const argv = require("./utils/default_yargs")
+  .option("masterSafe", {
+    type: "string",
+    describe: "Address of Gnosis Safe owning every bracket",
+    demandOption: true,
+  })
+  .option("outputFile", {
+    type: "string",
+    describe: "Path and file name for the output of the script",
+    demandOption: false,
+  }).argv
 
 module.exports = async (callback) => {
   try {
-    const fleetFactory = await FleetFactory.deployed()
-    const events = await fleetFactory.getPastEvents("FleetDeployed", {
-      filter: { owner: argv.masterSafe },
-      fromBlock: 0,
-      toBlock: "latest",
-    })
-    const bracketsAsObjects = events.map((object) => object.returnValues.fleet)
-    const bracketAddresses = [].concat(...bracketsAsObjects)
+    const bracketAddresses = await getDeployedBrackets(argv.masterSafe)
 
     // writing the brackets into a csv file
-    const createCsvWriter = require("csv-writer").createObjectCsvWriter
     const csvWriter = createCsvWriter({
-      path: "./bracket-addresses.csv",
-      header: [{ id: "bracketAddress", title: "bracket-address" }],
+      path: argv.outputFile || "./bracket-addresses.csv",
+      header: [
+        { id: "Address", title: "Address" },
+        { id: "Type", title: "Type" },
+        { id: "Description", title: "Description" },
+        { id: "Tags", title: "Tags" },
+        { id: "EL", title: "Etherscan link" },
+        { id: "CP", title: "Easy C&P for JSON/SQL" },
+      ],
     })
-    const bracketsFormatted = bracketAddresses.map((bracketAddress) => [{ bracketAddress: bracketAddress }])
-    const records = [].concat(...bracketsFormatted)
+
+    const exchange = await getExchange(web3)
+    const records = await Promise.all(
+      bracketAddresses.map(async (bracketAddress) => {
+        const orders = exchangeUtils.decodeOrdersBN(await exchange.getEncodedUserOrders.call(bracketAddress))
+        let tradingPair
+        if (orders.length > 0) {
+          const tokenInfoPromises = fetchTokenInfoFromExchange(exchange, [orders[0].buyToken, orders[0].sellToken])
+          tradingPair =
+            (await tokenInfoPromises[orders[0].sellToken]).symbol + " - " + (await tokenInfoPromises[orders[0].buyToken]).symbol
+        } else {
+          tradingPair = " - not yet defined -"
+        }
+        return {
+          Address: bracketAddress,
+          Type: "liquidity",
+          Description: "bracket-strategy",
+          Tags: "bracket-strategy on the pair " + tradingPair,
+          EL: "https://etherscan.io/address/" + bracketAddress,
+          CP: '"' + bracketAddress + '"',
+        }
+      })
+    )
     await csvWriter.writeRecords(records)
 
     callback()
