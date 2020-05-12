a9fa5ba6b96a286b92cf862cf5feda4ec4e3a4bd
Ben Smith <bh2smith@gmail.com>
2020-05-12 14:51:46 +0200

    typo

--- scripts/utils/sign_and_send.js
+++ scripts/utils/sign_and_send.js
@@ -41,7 +41,7 @@ module.exports = function (web3 = web3, artifacts = artifacts) {
     }
     const safeTxGas = await estimateGas(masterSafe, transaction)
     const baseGas = 0
-    console.log("Aquiring Transaction Hash")
+    console.log("Acquiring Transaction Hash")
     const transactionHash = await masterSafe.getTransactionHash(
       transaction.to,
       transaction.value,
