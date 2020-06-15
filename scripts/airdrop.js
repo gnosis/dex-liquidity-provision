54129c349c6f4e7d040aa916162dc4a4a84963ff
Ben Smith <bh2smith@gmail.com>
2020-06-15 16:31:35 +0200

    make test file have smaller amounts

--- scripts/airdrop.js
+++ scripts/airdrop.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises
 
-const { signAndSend } = require("./utils/gnosis_safe_server_interactions")(web3, artifacts)
+const { signAndSend, transactionExistsOnSafeServer } = require("./utils/gnosis_safe_server_interactions")(web3, artifacts)
 const { buildTransferDataFromList } = require("./utils/trading_strategy_helpers")(web3, artifacts)
 const { promptUser } = require("./utils/user_interface_helpers")
 const { default_yargs } = require("./utils/default_yargs")
@@ -14,6 +14,11 @@ const argv = default_yargs
     type: "string",
     describe: "file name (and path) to the list transfers",
     demandOption: true,
+  })
+  .option("verify", {
+    type: "boolean",
+    default: false,
+    describe: "Do not actually send transactions, just simulate their submission",
   })
   .option("useWei", {
     type: "string",
@@ -31,9 +36,14 @@ module.exports = async (callback) => {
     console.log("Preparing transaction data...")
     const transaction = await buildTransferDataFromList(masterSafe.address, transfers, argv.useWei, false, true)
 
-    const answer = await promptUser("Are you sure you want to send this transaction to the EVM? [yN] ")
-    if (answer == "y" || answer.toLowerCase() == "yes") {
-      await signAndSend(masterSafe, transaction, argv.network)
+    if (!argv.verify) {
+      const answer = await promptUser("Are you sure you want to send this transaction to the EVM? [yN] ")
+      if (answer == "y" || answer.toLowerCase() == "yes") {
+        await signAndSend(masterSafe, transaction, argv.network)
+      }
+    } else {
+      console.log("Verifying transaction")
+      await transactionExistsOnSafeServer(masterSafe, transaction, argv.network, (await masterSafe.nonce()).toNumber())
     }
 
     callback()
