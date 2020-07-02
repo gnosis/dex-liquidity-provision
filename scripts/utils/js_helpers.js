f5591fe3690ac3832a14c5b31db7d9354b460720
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-07-02 15:21:08 +0000

    Add function to retrieve output of quickest promise (#335)
    * Add returnFirstNotErroring
    
    * Test returnFirstNotErroring
    
    * Simpler handling
    
    * Add comment on multiple calls to resolve
    
    * Concatenate error strings with .join()

--- scripts/utils/js_helpers.js
+++ scripts/utils/js_helpers.js
@@ -18,7 +18,55 @@ const sleep = function (milliseconds) {
   return new Promise((r) => setTimeout(r, milliseconds))
 }
 
+/**
+ * Given an array of promises, returns a promise resolving to the
+ * output of the first resolving promise of the array. If any promise
+ * has been rejected in the meantime, this failure is ignored. If all
+ * promises are rejected, returns a rejecting promise returning an
+ * error containing all error messages concatenated with "&&".
+ * If entries of the input array are not promises, they will be treated
+ * as promises resolving to the input value.
+ *
+ * This function is meant to be replaced in the future by Promise.any,
+ * which is not yet availeble on Node.js.
+ * https://tc39.es/proposal-promise-any/#sec-promise.any
+ *
+ * Unlike Promise.any, if all input promises reject then the return
+ * value is a rejected promise returning an error with a message that
+ * concatenates all promise error messages (or output values, if
+ * message is not available) in the order they appear in the input
+ * array. (Promise.any returns an AggregateError.)
+ *
+ * @param {Promise[]} promiseArray array of promises (or values)
+ * @returns {Promise} promise returning the value returned by the
+ * input promise that is the quickest to resolve
+ */
+const returnFirstNotErroring = (promiseArray) =>
+  new Promise((resolve, reject) => {
+    let resolved = false
+    const errorPromises = []
+    for (const promise of promiseArray) {
+      errorPromises.push(
+        Promise.resolve(promise).then(
+          (result) => {
+            resolved = true
+            // only the first call to resolve() determines the outer promise output
+            resolve(result)
+          },
+          (error) => error
+        )
+      )
+    }
+    Promise.all(errorPromises).then((errorArray) => {
+      if (!resolved) {
+        const errorString = errorArray.map((error) => String(error.message || error)).join(" && ")
+        reject(new Error(errorString))
+      }
+    })
+  })
+
 module.exports = {
   uniqueItems,
   sleep,
+  returnFirstNotErroring,
 }
