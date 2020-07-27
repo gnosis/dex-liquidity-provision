6e87145b9736aece5b8e0f5adf0e9d3de8c56209
Benjamin Smith <bh2smith@users.noreply.github.com>
2020-07-27 10:11:19 +0200

    Airdrop from CSV (#377)
    * Include CSV as a possible file extension for airdrop
    Co-authored-by: Nicholas Rodrigues Lordello <nlordell@gmail.com>

--- scripts/airdrop.js
+++ scripts/airdrop.js
@@ -1,8 +1,11 @@
+const BigNumber = require("bignumber.js")
 const fs = require("fs").promises
+const path = require("path")
 
 const { signAndSend, transactionExistsOnSafeServer } = require("./utils/gnosis_safe_server_interactions")(web3, artifacts)
 const { buildTransferDataFromList } = require("./utils/trading_strategy_helpers")(web3, artifacts)
 const { promptUser, proceedAnyways } = require("./utils/user_interface_helpers")
+const { parseCsvFile } = require("./utils/parse_csv")
 const { default_yargs } = require("./utils/default_yargs")
 const argv = default_yargs
   .option("fundAccount", {
@@ -21,18 +24,46 @@ const argv = default_yargs
     describe: "Do not actually send transactions, just simulate their submission",
   }).argv
 
+const toPayment = function (leaderBoardItem) {
+  const { receiver, amount, token_address: tokenAddress } = leaderBoardItem
+  return {
+    tokenAddress,
+    receiver,
+    amount: new BigNumber(amount),
+  }
+}
+
+const parseTransferFile = async function (filename) {
+  const ext = path.extname(filename).toLowerCase()
+  let results
+  if (ext === ".csv") {
+    results = await parseCsvFile(filename)
+  } else if (ext === ".json") {
+    results = JSON.parse(await fs.readFile(filename, "utf8"))
+  } else {
+    throw new Error(`unsupported file type ${ext}`)
+  }
+  const payments = results.map(toPayment).filter((payment) => !payment.amount.isZero())
+  return payments.map(({ amount, receiver, tokenAddress }) => ({
+    amount: amount.toString(10),
+    receiver,
+    tokenAddress,
+  }))
+}
+
 module.exports = async (callback) => {
   try {
-    const GnosisSafe = artifacts.require("GnosisSafe")
-    const masterSafe = await GnosisSafe.at(argv.fundAccount)
+    const transfers = await parseTransferFile(argv.transferFile)
+    console.log(`Found ${transfers.length} valid elements in transfer file`)
 
-    const transfers = JSON.parse(await fs.readFile(argv.transferFile, "utf8"))
-    console.log(`Found ${transfers.length} elements in transfer file`)
     if (transfers.length > 200) {
       if (!(await proceedAnyways("It is not recommended to attempt more than 200 transfers."))) {
         callback("Error: Too many transfers!")
       }
     }
+
+    const GnosisSafe = artifacts.require("GnosisSafe")
+    const masterSafe = await GnosisSafe.at(argv.fundAccount)
 
     console.log("Preparing transaction data...")
     const transaction = await buildTransferDataFromList(masterSafe.address, transfers, false, true)
