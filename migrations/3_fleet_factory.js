6570b58a9e743f7cf45696d1277536dc3482f6ec
Andre Meyer <andre.meyer93@gmail.com>
2020-08-20 13:59:32 +0200

    add fleet factory deterministic

--- migrations/3_fleet_factory.js
+++ migrations/3_fleet_factory.js
@@ -1,6 +1,8 @@
 const GnosisSafeProxyFactory = artifacts.require("./GnosisSafeProxyFactory.sol")
 const FleetFactory = artifacts.require("./FleetFactory.sol")
+const FleetFactoryDeterministic = artifacts.require("./FleetFactoryDeterministic.sol")
 
 module.exports = async function (deployer) {
   await deployer.deploy(FleetFactory, GnosisSafeProxyFactory.address, { gas: 500000 })
+  await deployer.deploy(FleetFactoryDeterministic, GnosisSafeProxyFactory.address, { gas: 500000 })
 }
