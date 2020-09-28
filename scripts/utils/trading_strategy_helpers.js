d80da143c4a3284a192b57eaf2f252cff473b49b
Andre Meyer <andre.meyer93@gmail.com>
2020-09-28 13:43:49 +0200

    add missing genericFundMovement transaction

--- scripts/utils/trading_strategy_helpers.js
+++ scripts/utils/trading_strategy_helpers.js
@@ -1056,6 +1056,7 @@ module.exports = function (web3 = web3, artifacts = artifacts) {
     transactionsForOrders,
     transactionsForTransferApproveDepositFromList,
     transactionsForTransferApproveDepositFromOrders,
+    transactionGenericFundMovement,
     checkSufficiencyOfBalance,
     deployFleetOfSafes,
     fetchTokenInfoAtAddresses,
