08a0daef7b72fded25f64fa634c83f6f63e383f8
Felix Leupold <felixleupold90@gmail.com>
2020-10-14 19:16:40 +0200

    [Easy] Use packaged MultiSend contract instead of local deployment (#537)
    * [Easy] Use packaged MultiSend contract instead of local deployment
    
    * revert merge fuck up

--- migrations/2_dev_dependencies.js
+++ migrations/2_dev_dependencies.js
@@ -1,7 +1,6 @@
 const Artifactor = require("@truffle/artifactor")
 const migrateBatchExchange = require("@gnosis.pm/dex-contracts/src/migration/PoC_dfusion")
-const { GnosisSafe, GnosisSafeProxyFactory } = require("../scripts/utils/dependencies")(web3, artifacts)
-const MultiSend = artifacts.require("./MultiSend.sol")
+const { GnosisSafe, GnosisSafeProxyFactory, MultiSend } = require("../scripts/utils/dependencies")(web3, artifacts)
 
 module.exports = async function (deployer, network, accounts) {
   console.log("Migrating Batch Exchange")
@@ -17,18 +16,19 @@ module.exports = async function (deployer, network, accounts) {
   await artifactor.save(artefact)
 
   if (network === "development") {
-    await deploySafe(deployer)
-    await deployer.deploy(MultiSend)
+    await deploySafeContracts(deployer)
   } else {
     // eslint-disable-next-line no-console
     console.log("Not in development, so nothing to do. Current network is %s", network)
   }
 }
 
-async function deploySafe(deployer) {
+async function deploySafeContracts(deployer) {
   const artifactor = new Artifactor("node_modules/@gnosis.pm/safe-contracts/build/contracts/")
   await deployer.deploy(GnosisSafe)
   await artifactor.save(GnosisSafe)
   await deployer.deploy(GnosisSafeProxyFactory)
   await artifactor.save(GnosisSafeProxyFactory)
+  await deployer.deploy(MultiSend)
+  await artifactor.save(MultiSend)
 }
