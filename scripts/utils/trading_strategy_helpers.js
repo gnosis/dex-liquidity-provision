a1d1d861d9d53a1372546c327530ab02e4d4e5bc
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-10-26 18:30:43 +0100

    Remove spurious console.log

--- scripts/utils/trading_strategy_helpers.js
+++ scripts/utils/trading_strategy_helpers.js
@@ -1002,7 +1002,6 @@ module.exports = function (web3 = web3, artifacts = artifacts) {
     const tokenInfoPromises = fetchTokenInfoFromExchange(await exchangePromise, [baseTokenId, quoteTokenId])
     const createFleetTransaction = buildDeterministicFleetOfSafes(masterAddress, fleetSize, await noncePromise)
     const bracketAddresses = await calcSafeAddresses(fleetSize, await noncePromise)
-    console.log(bracketAddresses)
 
     const createOrderTransactions = transactionsForOrders(
       masterAddress,
