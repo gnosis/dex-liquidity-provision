6f97ea27b80212cf007dc1c5120cfa1f33c9a8fc
Andre Meyer <andre.meyer93@gmail.com>
2020-11-02 14:22:53 +0100

    fix pprettier

--- scripts/wrapper/withdraw.js
+++ scripts/wrapper/withdraw.js
@@ -137,11 +137,13 @@ module.exports = function (web3, artifacts) {
       }
     } else {
       amountFunction = async function (bracketAddress, tokenData, exchange) {
-        const amount = (await exchange.getBalance(bracketAddress, tokenData.address))
+        const amount = await exchange.getBalance(bracketAddress, tokenData.address)
         if (amount.gt(ZERO)) {
           return MAXUINT256
         } else {
-          log(`Skipping request for ${tokenData.symbol} on bracket ${bracketAddress} since there is no (0) balance for this token.`)
+          log(
+            `Skipping request for ${tokenData.symbol} on bracket ${bracketAddress} since there is no (0) balance for this token.`
+          )
           return ZERO
         }
       }
